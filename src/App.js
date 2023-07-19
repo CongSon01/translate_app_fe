43deb4910b20f73b188c272bb48a9cdc3894af96
congson01 <duongcongson01@gmail.com>
2023-07-19 16:33:53 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [inputText, setInputText] = useState('');
+  const [translatedText, setTranslatedText] = useState('');
+
+  const handleInputChange = (event) => {
+    setInputText(event.target.value);
+  };
+
+  const handleTranslate = async () => {
+    console.log("click me");
+    try {
+      const response = await fetch('http://localhost:5000/api/translate', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ text: inputText }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Translation request failed');
+      }
+
+      const data = await response.json();
+      setTranslatedText(data.translated_text);
+    } catch (error) {
+      console.error('Translation error:', error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Web Translate</h1>
+      <textarea
+        rows="5"
+        placeholder="Enter Vietnamese text..."
+        value={inputText}
+        onChange={handleInputChange}
+      />
+      <button onClick={handleTranslate}>Translate</button>
+      <p>Translated Text:</p>
+      <div>{translatedText}</div>
     </div>
   );
 }
